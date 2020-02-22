652bd5c91a980b8b408048d410119c29b124068f
jarronshih <1546735+jarronshih@users.noreply.github.com>
2020-02-21 22:49:51 -0800

    Download 109 data

--- data/download.py
+++ data/download.py
@@ -1,5 +1,7 @@
 import requests
+
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 from os import path
 from pathlib import Path
 from urllib.parse import urljoin, urlparse
@@ -12,12 +14,12 @@ def download(search_url: str, form_data: dict):
     table = search_resp_soup.find("table")
     rows = table.find_all("a")
 
-    for row in rows:
-        url = urljoin(search_url, row["href"])
+    def _thread_download_and_save(row_href: str):
+        url = urljoin(search_url, row_href)
         filename = Path(urlparse(url).path).name
 
         if path.exists(filename):
-            continue
+            return
 
         page = requests.get(url)
         page.encoding = "utf8"
@@ -25,19 +27,37 @@ def download(search_url: str, form_data: dict):
         with open(filename, "x") as f:
             f.write(page.text)
 
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        for row in rows:
+            executor.submit(_thread_download_and_save, row["href"])
+
 
 def download_108():
     # Entrance: https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/gsd_search_php.php?part=part_1
     search_url = "https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/ShowGsd.php"  # 108 year
-    payload = {"TxtSTest": "審查", "SubSTest": "依指定項目名稱查詢", "action": "SubSTest"}
+    payloads = [
+        {"TxtGsdCode": "0", "SubTxtGsdCode": "依校系代碼查詢", "action": "SubTxtGsdCode",},
+        {"TxtGsdCode": "1", "SubTxtGsdCode": "依校系代碼查詢", "action": "SubTxtGsdCode",},
+        {"TxtSTest": "審查", "SubSTest": "依指定項目名稱查詢", "action": "SubSTest"},
+    ]
 
-    download(search_url, payload)
+    for payload in payloads:
+        download(search_url, payload)
 
 
 def download_109():
-    pass
+    # Entrance: https://www.cac.edu.tw/apply109/system/109ColQrytk4p_forapply_os92k5w/gsd_search_php.php?part=part_1
+    search_url = "https://www.cac.edu.tw/apply109/system/109ColQrytk4p_forapply_os92k5w/ShowGsd.php"  # 108 year
+    payloads = [
+        {"TxtGsdCode": "0", "SubTxtGsdCode": "依校系代碼查詢", "action": "SubTxtGsdCode",},
+        {"TxtGsdCode": "1", "SubTxtGsdCode": "依校系代碼查詢", "action": "SubTxtGsdCode",},
+        {"TxtSTest": "審查", "SubSTest": "依指定項目名稱查詢", "action": "SubSTest"},
+    ]
+
+    for payload in payloads:
+        download(search_url, payload)
 
 
 if __name__ == "__main__":
-    download_108()
-    # download_109()
+    # download_108()
+    download_109()
