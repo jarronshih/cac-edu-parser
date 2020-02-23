9f6df162fbaaa3a82508b440de57557c4cc990af
jarronshih <1546735+jarronshih@users.noreply.github.com>
2020-02-22 21:52:04 -0800

    output reshape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from utils import zipfile_reader, school_parser
+from utils import zipfile_reader, school_parser, SchoolAttr
 
 
 def _zipfile_reader(filename):
@@ -8,12 +8,75 @@ def _zipfile_reader(filename):
     yield from itertools.islice(zipfile_reader(filename), 5)
 
 
-if __name__ == "__main__":
+def attr_analysis():
     attrs = defaultdict(int)
     for filename, content in zipfile_reader("data/109.zip"):
         d = school_parser(content)
-        # print(d)
         for name in d:
             attrs[name] += 1
 
-    # print(sorted(attrs.items(), key=lambda x: x[1], reverse=True))
+    return sorted(attrs.items(), key=lambda x: x[1], reverse=True)
+
+
+if __name__ == "__main__":
+    HEADERS = [
+        "校系代碼",
+        "校系名稱",
+        "學測成績佔甄選總成績比例",
+        "術科成績佔甄選總成績比例",
+        "審查資料",
+        "高中成績綜合審查",
+        "面試/口試/面談",
+        "All",
+    ]
+    with open("output.csv", "w+") as f:
+        # headers
+        f.write(",".join(HEADERS))
+        f.write("\n")
+
+        for filename, content in zipfile_reader("data/109.zip"):
+            result = school_parser(content)
+            row = [
+                result[SchoolAttr.INDEX],
+                result[SchoolAttr.NAME],
+            ]
+
+            if SchoolAttr.EXAM in result:
+                row.append(result[SchoolAttr.EXAM])
+            else:
+                row.append("")
+
+            if SchoolAttr.SPECIAL_EXAM in result:
+                row.append(result[SchoolAttr.SPECIAL_EXAM])
+            else:
+                row.append("")
+
+            if "審查資料" in result:
+                row.append(result["審查資料"])
+            else:
+                row.append("")
+
+            if "高中成績綜合審查" in result:
+                row.append(result["高中成績綜合審查"])
+            else:
+                row.append("")
+
+            for key in result:
+                if not isinstance(key, str):
+                    continue
+                if "面試" in key or "口試" in key or "面談" in key:
+                    row.append(result[key])
+                    break
+            else:
+                row.append("")
+
+            row.append(
+                ";".join(
+                    f"{k}:{v}"
+                    for k, v in result.items()
+                    if isinstance(v, str) and v[-1] == "%"
+                )
+            )
+
+            f.write(",".join(row))
+            f.write("\n")
--- utils.py
+++ utils.py
@@ -4,10 +4,13 @@ from zipfile import ZipFile
 
 
 class SchoolAttr(Enum):
-    INDEX = auto()
-    NAME = auto()
-    EXAM = auto()
-    SPECIAL_EXAM = auto()
+    INDEX = "校系代碼"
+    NAME = "校系名稱"
+    EXAM = "學測成績"
+    SPECIAL_EXAM = "術科成績"
+
+    def __str__(self):
+        return str(self.value)
 
 
 def zipfile_reader(zip_file_path: str):
@@ -26,45 +29,46 @@ def school_parser(content: str):
     # index
     index = tables[1].find_all("td", limit=2)[1].text.strip()
     assert index
-    result[SchoolAttr.INDEX.name.lower()] = index
-
-    # exam
-    exam_tds = tables[1].find("tr").find_all("td", limit=7)
-    exam = exam_tds[6].text.strip()
-    assert "%" in exam, f"Exam not found: {exam_tds}"
-    result[SchoolAttr.EXAM.name.lower()] = exam
+    result[SchoolAttr.INDEX] = index
 
     # name
     school_name_raw = tables[0].find("p").text
     school_name = "".join(school_name_raw.split())
     assert school_name, f"School Name not found: {school_name_raw}"
-    result[SchoolAttr.NAME.name.lower()] = school_name
+    result[SchoolAttr.NAME] = school_name
+
+    # exam
+    exam_tds = tables[1].find("tr").find_all("td", limit=7)
+    exam = exam_tds[6].text.strip()
+    assert "%" in exam, f"Exam not found: {exam_tds}"
+    if exam != "0%":
+        result[SchoolAttr.EXAM] = exam
 
     # special exam
     special_exam_tds = tables[1].find("tr").find_all("td")
     if len(special_exam_tds) >= 15:
         special_exam = special_exam_tds[14].text.strip()
         if "%" in special_exam and special_exam[:-1]:
-            result[SchoolAttr.SPECIAL_EXAM.name.lower()] = special_exam
+            result[SchoolAttr.SPECIAL_EXAM] = special_exam
 
     # others
     for tr in tables[1].find_all("tr"):
         tds = tr.find_all("td")
         for i in range(8, min(10, len(tds))):
             percentage = tds[i].text.strip()
-            if "%" not in percentage and percentage[:-1]:
+            if "%" not in percentage:
+                continue
+            if not percentage[:-1] or percentage == "0%":
                 continue
             attr = tds[i - 2].text.strip()
             result[attr] = percentage
             break
 
-    # assert 100%
+    # validate total percentage is 100%
     total = 0
     for value in result.values():
         if "%" in value:
             total += int(value[:-1])
-    if total != 100:
-        print(result)
     assert total == 100, result
 
     return result
